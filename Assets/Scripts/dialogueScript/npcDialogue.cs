1d3b32be0c85abdb2043d3ac6242327fe4618078
EnochHimesaka <lwl934561097@gmail.com>
2025-02-09 20:45:56 -0500

    1
    1

---
+++ Assets/Scripts/dialogueScript/npcDialogue.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class npcDialogue : MonoBehaviour
+{
+    public Dialogue dialogue;
+    //public bool isDialogue = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (transform.parent.tag)
+        {
+            case "npc1":
+              
+                DialogueManager.id = 1;
+                break;
+            case "npc2":
+                DialogueManager.id = 2;
+                break;
+
+        }
+        if (other.CompareTag("Player"))// && isDialogue == false
+        {
+           
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            //isDialogue = true;
+        }
+    }
+}
